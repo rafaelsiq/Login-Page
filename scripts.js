85ee17b51ae190dc7ff06b3fd08b585888edba64
rafaelsiq <rafaelsqf2@gmail.com>
2019-12-26 19:54:46 +0000

    26/12/2019 Rafael siqueira de Freitas
    Signed-off-by: rafaelsiq <rafaelsqf2@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,14 @@
+       var nome;
+       var email;
+       var senha;
+       var confirmasenha;
+       var contador = 0;
+   function minhafuncao(){
+       contador++;
+       if(contador < 5){
+           alert("É possivel que senha digitada seja fraca ou não coincidem.\nSenhas fortes contêm letras maiusculas e minusculas, numeros e caracteres especiais.")
+       }
+       else{
+           alert("Tudo certo!\nVocê já pode navegar a vontade! ")
+       }        
+}
